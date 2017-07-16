3c4e3b1c7ad82ba4887495060c874faba254cd48
ShiraOzeri <shira.ozeri@mobiletornado.com>
2017-07-16 15:22:38 +0300

    try with no error

--- app/src/test/java/com/example/sozeri/try_mytest/ExampleUnitTest.java
+++ app/src/test/java/com/example/sozeri/try_mytest/ExampleUnitTest.java
@@ -26,6 +26,6 @@ public class ExampleUnitTest {
     }
     @Test
     public void addition_isCorrect3() throws Exception {
-        assertEquals(6, 4 + 3);
+        assertEquals(7, 4 + 3);
     }
 }
